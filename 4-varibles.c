e5d101ab0bf91f961c7fa8cec2e35438345ea7bf
Ritik Kumar <98963769+krritik01@users.noreply.github.com>
2022-04-13 23:37:59 +0530

    Add files via upload

---
+++ 4-varibles.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+
+    int a = 5;
+    int e = 9;
+    // int a=5 represents interger value and if we use int a =9.9 then it shows error bacause 9.9 is real value and for that we use float
+
+    float b = 7.8;
+
+    char c = 'u';
+
+    int d = 8;
+
+    printf("The value of a is %d \n", a);
+
+    printf("The value of b is %c \n", c);
+
+    printf("The value of c is %d\n", e);
+
+    printf("Sum of a and d is %d", a + d+e);
+    return 0;
+}
