5e23c2476dae28c4db26e3b8e54463eeadc8f86b
Ritik Kumar <98963769+krritik01@users.noreply.github.com>
2023-01-23 15:51:06 +0530

    Add files via upload

---
+++ 11-oper_prec.c
@@ -0,0 +1,11 @@
+// Operator Precedence
+#include<stdio.h>
+
+int main(){
+    int x = 4;
+    int y = 6;
+
+    printf("The value of 5*x +7*y is %d\n", 5*x+7*y);
+    printf("The value of 7*y/5*x is %d\n ", 7*y/5*x);
+    return 0;
+}
